a276d3e7dbba5ac5d5b631618679d54096a895d1
Giovanni <7008900+sinkingsugar@users.noreply.github.com>
2021-06-23 11:32:57 +0900

    Add Fragcolor storage pallet template

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -40,7 +40,7 @@ pub use frame_support::{
 use pallet_transaction_payment::CurrencyAdapter;
 
 /// Import the template pallet.
-pub use pallet_template;
+pub use frag_pallet_storage;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -275,7 +275,7 @@ impl pallet_sudo::Config for Runtime {
 }
 
 /// Configure the pallet-template in pallets/template.
-impl pallet_template::Config for Runtime {
+impl frag_pallet_storage::Config for Runtime {
 	type Event = Event;
 }
 
@@ -295,7 +295,7 @@ construct_runtime!(
 		TransactionPayment: pallet_transaction_payment::{Pallet, Storage},
 		Sudo: pallet_sudo::{Pallet, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the pallet-template in the runtime.
-		TemplateModule: pallet_template::{Pallet, Call, Storage, Event<T>},
+		TemplateModule: frag_pallet_storage::{Pallet, Call, Storage, Event<T>},
 	}
 );
 
