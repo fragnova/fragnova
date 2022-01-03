b41cd14aef8487701a7425f968f6a1b10c7941ad
Giovanni <7008900+sinkingsugar@users.noreply.github.com>
2022-01-03 13:58:48 +0900

    do it ourself ipfs setup

--- chains/run.sh
+++ chains/run.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+wget https://dist.ipfs.io/go-ipfs/v0.10.0/go-ipfs_v0.10.0_linux-amd64.tar.gz
+tar -xvzf go-ipfs_v0.10.0_linux-amd64.tar.gz
 PATH=$PATH:/data/go-ipfs
 ipfs init
 ipfs daemon &
