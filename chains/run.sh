996794c31b3339230a708b1a0c28045a349bdac9
Giovanni <7008900+sinkingsugar@users.noreply.github.com>
2022-01-03 16:21:02 +0900

    use curl

--- chains/run.sh
+++ chains/run.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-wget https://dist.ipfs.io/go-ipfs/v0.10.0/go-ipfs_v0.10.0_linux-amd64.tar.gz
+curl -O https://dist.ipfs.io/go-ipfs/v0.10.0/go-ipfs_v0.10.0_linux-amd64.tar.gz
 tar -xvzf go-ipfs_v0.10.0_linux-amd64.tar.gz
 PATH=$PATH:/data/go-ipfs
 ipfs init
