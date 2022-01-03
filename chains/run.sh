9cfe8b67f099041391bcfe8f3c1c19ec602b64da
Giovanni <7008900+sinkingsugar@users.noreply.github.com>
2022-01-03 17:14:43 +0900

    add nonces

--- chains/run.sh
+++ chains/run.sh
@@ -1,8 +1,13 @@
 #/bin/bash
 
+apt-get update
+apt-get install -y curl
+
 curl -O https://dist.ipfs.io/go-ipfs/v0.10.0/go-ipfs_v0.10.0_linux-amd64.tar.gz
 tar -xvzf go-ipfs_v0.10.0_linux-amd64.tar.gz
 PATH=$PATH:/data/go-ipfs
+
 ipfs init
 ipfs daemon &
+
 cbl /data/chains/fragments-tests.edn
