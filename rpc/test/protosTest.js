64570b594cc59b1c7b033f3f13a8715fcfe1f475
Giovanni <7008900+sinkingsugar@users.noreply.github.com>
2022-05-13 09:51:52 +0800

    fix rpc test further

--- rpc/test/protosTest.js
+++ rpc/test/protosTest.js
@@ -59,7 +59,7 @@ describe('Protos RPCs', () => {
         it('should return correct owner', async () => {
 
             const params = api.createType("GetProtosParams", {desc: true, from: 0, limit: 10, metadata_keys: ['A', 'A'],
-                                                              owner: "5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy", categories: ["Chain"], return_owners: true});
+                                                              owner: "5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy", categories: [{"chain": "generic"}], return_owners: true});
 
 
             let result = await api.rpc.protos.getProtos(params)
