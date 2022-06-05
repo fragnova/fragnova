1f569a4a0d5089baea470c69796fe725f2b94445
Giovanni <7008900+sinkingsugar@users.noreply.github.com>
2022-06-05 21:50:35 +0800

    build fixes

--- pallets/fragments/src/tests.rs
+++ pallets/fragments/src/tests.rs
@@ -1,11 +1,8 @@
-use crate::{mock::*, Error, FragmentMetadata, Fragments, Proto2Fragments};
+use crate::{mock::*, Error, FragmentMetadata, Classes, Proto2Fragments};
 use codec::Encode;
 use frame_support::{assert_noop, assert_ok};
-use pallet_protos::LinkedAsset;
 use protos::categories::{Categories, TextCategories};
 use protos::permissions::FragmentPerms;
-use sp_clamor::Hash256;
-use sp_core::Pair;
 use sp_io::hashing::blake2_256;
 
 fn initial_upload() {
@@ -28,7 +25,7 @@ fn create_should_works() {
 	new_test_ext().execute_with(|| {
 		initial_upload();
 
-		let fragment_data = FragmentMetadata { name: "name".as_bytes().to_vec(), currency: 0 };
+		let fragment_data = FragmentMetadata { name: "name".as_bytes().to_vec(), currency: None };
 
 		let hash = blake2_256(
 			&[&PROTO_HASH[..], &fragment_data.name.encode(), &fragment_data.currency.encode()]
@@ -43,7 +40,7 @@ fn create_should_works() {
 			true,
 			None
 		));
-		assert!(Fragments::<Test>::contains_key(&hash));
+		assert!(Classes::<Test>::contains_key(&hash));
 		assert!(Proto2Fragments::<Test>::contains_key(&PROTO_HASH));
 	});
 }
@@ -51,7 +48,7 @@ fn create_should_works() {
 #[test]
 fn create_should_not_work_if_protos_not_found() {
 	new_test_ext().execute_with(|| {
-		let fragment_data = FragmentMetadata { name: "name".as_bytes().to_vec(), currency: 0 };
+		let fragment_data = FragmentMetadata { name: "name".as_bytes().to_vec(), currency: None };
 
 		assert_noop!(
 			FragmentsPallet::create(
@@ -77,7 +74,7 @@ fn create_should_not_work_if_proto_owner_not_found() {
 			96, 79, 173, 215, 209, 136, 226, 220, 88, 91, 78, 26, 251,
 		];
 
-		let fragment_data = FragmentMetadata { name: "name".as_bytes().to_vec(), currency: 0 };
+		let fragment_data = FragmentMetadata { name: "name".as_bytes().to_vec(), currency: None };
 
 		assert_noop!(
 			FragmentsPallet::create(
@@ -98,7 +95,7 @@ fn create_should_not_work_if_fragment_already_exist() {
 	new_test_ext().execute_with(|| {
 		initial_upload();
 
-		let fragment_data = FragmentMetadata { name: "name".as_bytes().to_vec(), currency: 0 };
+		let fragment_data = FragmentMetadata { name: "name".as_bytes().to_vec(), currency: None };
 
 		let hash = blake2_256(
 			&[&PROTO_HASH[..], &fragment_data.name.encode(), &fragment_data.currency.encode()]
@@ -113,7 +110,7 @@ fn create_should_not_work_if_fragment_already_exist() {
 			true,
 			None
 		));
-		assert!(Fragments::<Test>::contains_key(&hash));
+		assert!(Classes::<Test>::contains_key(&hash));
 
 		assert_noop!(
 			FragmentsPallet::create(
