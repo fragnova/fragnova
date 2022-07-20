ab45b14a42cc253247fa9b598f5ec736a80ae2af
aang114 <karandevsapra@gmail.com>
2022-07-20 22:42:26 +0800

    if intended, copied fragment instance should expire earlier

--- pallets/fragments/src/lib.rs
+++ pallets/fragments/src/lib.rs
@@ -821,8 +821,10 @@ pub mod pallet {
 
 				// handle expiration
 				if let Some(expiring_at) = item_data.expiring_at {
-					let expiration = if let Some(expiration) = expiration {
+					let expiration = 
+					if let Some(expiration) = expiration {
 						if expiration < expiring_at {
+							item_data.expiring_at = Some(expiration); 
 							expiration
 						} else {
 							expiring_at
@@ -832,6 +834,7 @@ pub mod pallet {
 					};
 					<Expirations<T>>::append(expiration, (class, Compact(edition), Compact(copy)));
 				} else if let Some(expiration) = expiration {
+					item_data.expiring_at = Some(expiration); 
 					<Expirations<T>>::append(expiration, (class, Compact(edition), Compact(copy)));
 				}
 
