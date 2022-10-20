0d2de4ecc3628cf3322481a65adc24ef3c95797f
Alessandro Baffa <alessandro.baffa@gmail.com>
2022-10-20 10:54:27 +0900

    fix

--- shards/run.sh
+++ shards/run.sh
@@ -1,13 +1,7 @@
 #/bin/bash
 
 apt-get update
-sudo apt-get install -y ca-certificates curl gnupg lsb-release wget
-sudo mkdir -p /etc/apt/keyrings
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install -y wget
 
 cd /data/shards
 
@@ -24,4 +18,5 @@ ipfs daemon &
 sleep 5 &&
 
 # we use this Docker container: https://hub.docker.com/r/chainblocks/shards
-docker run --rm --user root --network host -v `pwd`:/la-casa chainblocks/shards shards /la-casa/shards-new/run-test-ipfs.edn  #shards test-protos-ipfs.edn
+#docker run --rm --user root --network host -v `pwd`:/la-casa chainblocks/shards shards /la-casa/shards-new/run-test-ipfs.edn  #shards test-protos-ipfs.edn
+shards shards-new/run-test-ipfs.edn
