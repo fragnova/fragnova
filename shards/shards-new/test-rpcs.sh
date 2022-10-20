2731f1dd6f5b8180dfc190cefcc647233ffea4d2
aang114 <karandevsapra@gmail.com>
2022-10-21 02:13:04 +0800

    bugfix

--- shards/shards-new/test-rpcs.sh
+++ shards/shards-new/test-rpcs.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
+set -e
 
 ROOT=`pwd`
 echo "ROOT is $ROOT"
 
-cd $ROOT/shards/shards-new &&
-echo "ls is:"
-ls &&
+cd $ROOT/shards/shards-new
 
 # we use this Docker container: https://hub.docker.com/r/chainblocks/shards
-docker run --rm --user root --network host --volume `pwd`:/HOME chainblocks/shards shards /HOME/run-create-proto-and-fragment.edn && # shards run-create-proto-and-fragment.edn &&
+docker run --rm --user root --network host --volume `pwd`:/HOME chainblocks/shards shards /HOME/run-create-proto-and-fragment.edn && # shards run-create-proto-and-fragment.edn
 
-echo && sleep 10 &&
+echo && sleep 10
 
-cd $ROOT/rpc &&
-npm install &&
+cd $ROOT/rpc
+npm install
 npm test
--- shards/test-ipfs.sh
+++ shards/test-ipfs.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
+set -e
 
 # INSTALL IPFS
 apt-get update
 sudo apt-get install -y wget
-wget -q https://dist.ipfs.tech/kubo/v0.16.0/kubo_v0.16.0_linux-amd64.tar.gz &&
-tar -xvzf kubo_v0.16.0_linux-amd64.tar.gz &&
-export PATH=$PATH:`pwd`/kubo &&
-
-# GO TO "shards" FOLDER
-cd shards &&
-echo "ls is:"
-ls &&
+wget -q https://dist.ipfs.tech/kubo/v0.16.0/kubo_v0.16.0_linux-amd64.tar.gz
+tar -xvzf kubo_v0.16.0_linux-amd64.tar.gz
+export PATH=$PATH:`pwd`/kubo
 
 # RUN IPFS
-ipfs init && # Initializes ipfs configuration files and generates a new keypair. (https://docs.ipfs.tech/how-to/command-line-quick-start/#initialize-the-repository)
-ipfs config profile apply test && # we are using the "test" profile, which "Reduces external interference of IPFS daemon. Useful when using the daemon in test environments." (https://docs.ipfs.tech/how-to/default-profile/#available-profiles)
+ipfs init # Initializes ipfs configuration files and generates a new keypair. (https://docs.ipfs.tech/how-to/command-line-quick-start/#initialize-the-repository)
+ipfs config profile apply test # we are using the "test" profile, which "Reduces external interference of IPFS daemon. Useful when using the daemon in test environments." (https://docs.ipfs.tech/how-to/default-profile/#available-profiles)
 ipfs daemon & # The daemon will start listening on ports on the network, which are documented in (and can be modified through) 'ipfs config Addresses'
-sleep 5 &&
+sleep 5
 
-# RUN COMMAND `shards /HOME/run-test-ipfs.edn` IN DOCKER CONTAINER
-docker run --rm --user root --network host --volume `pwd`:/HOME chainblocks/shards shards /HOME/run-test-ipfs.edn  # we use this Docker container: https://hub.docker.com/r/chainblocks/shards
+# RUN SHARDS SCRIPT "run-test-ipfs.edn"
+shards /DUMMY-FOLDER/run-test-ipfs.edn  # we use this Docker container: https://hub.docker.com/r/chainblocks/shards
