4cfe46c4f0aea477d5ba30cee6e54a782390d054
Alessandro Baffa <alessandro.baffa@gmail.com>
2022-10-20 10:26:11 +0900

    install docker

--- shards/run.sh
+++ shards/run.sh
@@ -1,7 +1,13 @@
 #/bin/bash
 
 apt-get update
-apt-get install -y wget
+sudo apt-get install -y ca-certificates curl gnupg lsb-release wget
+sudo mkdir -p /etc/apt/keyrings
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 cd /data/shards
 
