861e56654bea29ebcbe881122a3fa3ebe9710960
aang114 <karandevsapra@gmail.com>
2023-03-13 09:08:44 +0800

    update fragnova primitives

--- primitives/fragnova/src/lib.rs
+++ primitives/fragnova/src/lib.rs
@@ -3,26 +3,6 @@
 // Ensure we're `no_std` when compiling for Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-// #[cfg(feature = "std")]
-// extern crate chainblocks;
-
-/// Types that will be used by the Protos pallet
-pub mod protos;
-/// Types that will be used by the Fragments pallet
-pub mod fragments;
-/// Helper Functions that can be used in other packages of this workspace
-mod helper_functions;
-pub use helper_functions::{
-	http_json_post,
-	get_locked_frag_account,
-	get_account_id,
-};
-/// TODO - Documentation
-mod runtime_interface;
-pub use runtime_interface::fragnova;
-#[cfg(feature = "std")]
-pub use runtime_interface::init;
-
 /// 64 bytes u8-Array
 pub type Hash64 = [u8; 8];
 /// 128 bytes u8-Array
@@ -39,3 +19,27 @@ pub type Hash256 = [u8; 32];
 /// 0xa0e402 is the Multihash code for blake2b-256 (https://github.com/multiformats/multihash)
 /// 0x20 is the length of the digest
 pub const CID_PREFIX: [u8; 6] = hex_literal::hex!("0155a0e40220");
+
+
+/// Types that will be used by the Protos pallet
+pub mod protos;
+/// Types that will be used by the Fragments pallet
+pub mod fragments;
+
+/// Helper Functions that can be used in other packages of this workspace
+#[cfg(feature = "fragnova-workspace")]
+mod helper_functions;
+#[cfg(feature = "fragnova-workspace")]
+pub use helper_functions::{
+	http_json_post,
+	get_locked_frag_account,
+	get_account_id,
+};
+/// TODO - Documentation
+#[cfg(feature = "fragnova-workspace")]
+mod runtime_interface;
+#[cfg(feature = "fragnova-workspace")]
+pub use runtime_interface::fragnova;
+#[cfg(feature = "fragnova-workspace")]
+#[cfg(feature = "std")]
+pub use runtime_interface::init;
