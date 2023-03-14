f4616fadc148a1a38f442d9fb07cdccb4e435cb9
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2023-03-14 09:19:25 +0800

    spec for mainnet

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -186,15 +186,15 @@ pub mod opaque {
 ///   https://docs.substrate.io/v3/runtime/upgrades#runtime-versioning
 #[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("fragnova-testnet"),
-	impl_name: create_runtime_str!("fragnova-color"),
+	spec_name: create_runtime_str!("fragnova"),
+	impl_name: create_runtime_str!("fragnova-hstn"),
 	authoring_version: 1,
 	// The version of the runtime specification. A full node will not attempt to use its native
 	//   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
-	spec_version: 6,
+	spec_version: 1,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
